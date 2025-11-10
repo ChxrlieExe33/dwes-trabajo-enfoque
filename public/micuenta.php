1bc3e20dae4eef52687b20e4808a765c51ef5fb9
Charles <charliedavidcrane13@gmail.com>
2025-11-10 16:41:07 +0100

    Made the micuenta page mobile friendly

--- public/micuenta.php
+++ public/micuenta.php
@@ -38,27 +38,27 @@ $userData = UserService::getUserData($_SESSION['user_id']);
 
     <?php include_once 'navbar.php'; ?>
 
-    <form class="w-full px-[25%] py-6 flex flex-col items-center justify-start gap-4 pb-12" method="post">
+    <form class="w-full px-4 md:px-[25%] py-6 flex flex-col items-center justify-start gap-4 pb-12" method="post">
 
         <h1 class="text-2xl font-bold">Datos personales</h1>
 
         <p><b>Correo:</b> <?php echo $userData->getEmail(); ?></p>
 
-        <section class="px-20 w-full flex flex-col items-start justify-items-start">
+        <section class="px-4 md:px-20 w-full flex flex-col items-start justify-items-start">
 
             <label for="nombre" class="font-bold">Nombre:</label>
             <input id="nombre" type="text" name="nombre" value="<?php echo $userData->getNombre(); ?>" placeholder="Tu nombre..." class="w-full px-6 py-2 border-1 border-gray-300/90 rounded-2xl shadow-lg">
 
         </section>
 
-        <section class="px-20 w-full flex flex-col items-start justify-items-start">
+        <section class="px-4 md:px-20 w-full flex flex-col items-start justify-items-start">
 
             <label for="apellidos" class="font-bold">Apellidos:</label>
             <input id="apellidos" type="text" name="apellidos" value="<?php echo $userData->getApellidos(); ?>" placeholder="Tus apellidos..." class="w-full px-6 py-2 border-1 border-gray-300/90 rounded-2xl shadow-lg">
         
         </section>
 
-        <section class="px-20 w-full flex flex-col items-center justify-items-start">
+        <section class="px-4 md:px-20 w-full flex flex-col items-center justify-items-start">
 
             <label for="fecha_nac" class="font-bold">Fecha nacimiento:</label>
             <input id="fecha_nac" type="date" name="fecha_nac" value="<?php echo $userData->getFechaNacimiento(); ?>" class="px-6 py-2 border-1 border-gray-300/90 rounded-2xl shadow-lg">
@@ -69,21 +69,21 @@ $userData = UserService::getUserData($_SESSION['user_id']);
 
         <h1 class="text-2xl font-bold">Datos de entrega</h1>
 
-        <section class="px-20 w-full flex flex-col items-start justify-items-start">
+        <section class="px-4 md:px-20 w-full flex flex-col items-start justify-items-start">
 
             <label for="d_entrega" class="font-bold">Dirección:</label>
             <input id="d_entrega" type="text" name="direccion_entrega" value="<?php echo $userData->getDireccionEntrega(); ?>" placeholder="Direccion entrega..." class="w-full px-6 py-2 border-1 border-gray-300/90 rounded-2xl shadow-lg">
 
         </section>   
 
-        <section class="px-20 w-full flex flex-col items-start justify-items-start">
+        <section class="px-4 md:px-20 w-full flex flex-col items-start justify-items-start">
 
             <label for="c_entrega" class="font-bold">Ciudad:</label>
             <input id="c_entrega" type="text" name="ciudad_entrega" value="<?php echo $userData->getCiudadEntrega(); ?>" placeholder="Ciudad entrega..." class="w-full px-6 py-2 border-1 border-gray-300/90 rounded-2xl shadow-lg">
         
         </section>   
 
-        <section class="px-20 w-full flex flex-col items-start justify-items-start">
+        <section class="px-4 md:px-20 w-full flex flex-col items-start justify-items-start">
 
             <label for="p_entrega" class="font-bold">Provincia:</label>
             <input id="p_entrega" type="text" name="provincia_entrega" value="<?php echo $userData->getProvinciaEntrega(); ?>" placeholder="Provincia entrega..." class="w-full px-6 py-2 border-1 border-gray-300/90 rounded-2xl shadow-lg">
@@ -95,7 +95,7 @@ $userData = UserService::getUserData($_SESSION['user_id']);
 
         <h1 class="text-2xl font-bold">Datos de facturación</h1>
 
-        <section class="px-20 w-full flex flex-col items-start justify-items-start">
+        <section class="px-4 md:px-20 w-full flex flex-col items-start justify-items-start">
 
             <label for="d_fac" class="font-bold">Dirección:</label>
             <input id="d_fac" type="text" name="direccion_facturacion" value="<?php echo $userData->getDireccionFacturacion(); ?>" placeholder="Direccion facturacion..." class="w-full px-6 py-2 border-1 border-gray-300/90 rounded-2xl shadow-lg">
@@ -103,7 +103,7 @@ $userData = UserService::getUserData($_SESSION['user_id']);
         
         </section>   
 
-        <section class="px-20 w-full flex flex-col items-start justify-items-start">
+        <section class="px-4 md:px-20 w-full flex flex-col items-start justify-items-start">
 
             <label for="c_fac" class="font-bold">Ciudad:</label>
             <input id="c_fac" type="text" name="ciudad_facturacion" value="<?php echo $userData->getCiudadFacturacion(); ?>" placeholder="Ciudad facturacion..." class="w-full px-6 py-2 border-1 border-gray-300/90 rounded-2xl shadow-lg">
@@ -111,7 +111,7 @@ $userData = UserService::getUserData($_SESSION['user_id']);
         
         </section>   
 
-        <section class="px-20 w-full flex flex-col items-start justify-items-start">
+        <section class="px-4 md:px-20 w-full flex flex-col items-start justify-items-start">
 
             <label for="p_fac" class="font-bold">Provincia:</label>
             <input id="p_fac" type="text" name="provincia_facturacion" value="<?php echo $userData->getProvinciaFacturacion(); ?>" placeholder="Provincia facturacion..." class="w-full px-6 py-2 border-1 border-gray-300/90 rounded-2xl shadow-lg">
